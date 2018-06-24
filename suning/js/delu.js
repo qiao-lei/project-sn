bdb22217fd33776b5dfe9be7575135253aa0b79d
qiaolei <tsql333@126.com>
2018-06-24 12:02:41 +0800

    登录基本实现

--- suning/js/delu.js
+++ suning/js/delu.js
@@ -11,7 +11,7 @@ $(function(){
 	});
 	$("#password").focus(function(){
 		if(!regname.test($(this).val())){
-			$("#errorpassword").text("请输入以字母、数字、_开头的6到16位字符，不支持中文").css("color","#FF4400");
+			$("#errorpassword").text("请输入以字母、数字、_开头的5到16位字符，不支持中文").css("color","#FF4400");
 		}
 	});
 	$("#username").focusout(function(){
@@ -47,6 +47,69 @@ $(function(){
 		}else{
 			$("#errorname").text("用户名不能为空！").css("color","red");
 		}
+	});
+//登录验证；wx_dl  zh_dl
+	
+	$(".login_top a").eq(0).click(function(){
+		$(this).addClass("checkon").siblings().removeClass("checkon");
+		$(".wx_dl").show();
+		$(".zh_dl").hide();
+	});
+	$(".login_top a").eq(1).click(function(){
+		$(this).addClass("checkon").siblings().removeClass("checkon");
+		$(".wx_dl").hide();
+		$(".zh_dl").show();
+	});
+	
+	$("#dlbtn").click(function(){
+		for(var i=0;i<jsonzc.length;i++){
+			if($("#userdl").val()==jsonzc[i]["user"]){
+				$("#dlerrorname").text("");
+				if($("#passworddl").val()==jsonzc[i]["pass"]){
+					location.href="../index.html";
+					
+					break;
+				}else{
+					$("#dlerrorpassword").text("密码不对!").css("color","red");
+					break;
+				}
+			}else{
+				$("#dlerrorname").text("此用户不存在！").css("color","red");
+			}
+		}
+	});
+	$("#userdl").focus(function(){
+		$("#dlerrorname").text("");
+		$("#dlname label").css("background-position","-82px -67px")
+		.next().removeAttr("placeholder");
+		
+	});
+	$("#userdl").blur(function(){
+		$("#dlname label").css("background-position","-123px -67px");
+	});
+	$("#passworddl").focus(function(){
+		$("#dlerrorpassword").text("");
+		$("#dlpassword label").css("background-position","-41px -67px")
+		.next().removeAttr("placeholder");
+	});
+	$("#passworddl").blur(function(){
+		$("#dlpassword label").css("background-position","0 -108px");
+	});
+	//div id="dlname" id="userdl" id="dlerrorname"
+	//<div id="dlpassword" id="passworddl" id="dlerrorpassword"
+	$("#dl_wang a").eq(0).click(function(){
+		$("#dlerrorname").text("");
+		$(this).hide().next().show();
+		$("#userdl").attr({placeholder:"手机号码"});
+		$("#passworddl").attr({placeholder:"短信验证码"}).parent()
+		.append("<a href='#'>获取验证码</a>");
+	});
+	$("#dl_wang a").eq(1).click(function(){
+		$("#dlerrorname").text("");
+		$(this).hide().prev().show();
+		$("#passworddl").parent().find("a").remove();
+		$("#userdl").attr({placeholder:"用户名/QQ/微信/易付宝"});
+		$("#passworddl").attr({placeholder:"密码"});
 	});
 });
 
