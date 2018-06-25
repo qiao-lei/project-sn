d2856ed5fe7937eb354fad10b1bdd08425e95653
qiaolei <tsql333@126.com>
2018-06-25 19:06:59 +0800

    update index jsonp

--- dist/js/snjs.js
+++ dist/js/snjs.js
@@ -124,4 +124,19 @@ $(function(){
 	$(".hoverul").mouseout(function(){
 		$(this).children().eq(1).hide();
 	});
-})
+	$.ajax({
+		type:"get",
+		url:"https://lib.suning.com/homepage/model/homepage1_326191_lazyload326191.json",
+		dataType:"jsonp",
+		jsonpCallback:"lazyload326191",
+		async:true,
+		success:function(data){
+			$(".product_wrap").append(data["data"]);
+			$("img").each(function(){
+			$(this).attr("src",$(this).attr("lazy-src"));
+	});
+		}
+	});
+	
+	
+});
